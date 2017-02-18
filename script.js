b036553b3ca44a19fd25de82dc116717d65f8780
ffsommers <ssommers21@gmail.com>
2017-02-17 16:43:48 -0800

    fixes improper capture of select option values for game settings

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
 $(document).ready(function(){
+	var settings = {
+	difficulty: 1,
+	rounds: 1
+};
 	console.log("ready");
-	$("#begin").on("click", function(){
-    	console.log("click");
-     	// fetchWords();
-     	getWords();
+	$("#play").on("click", function(){
+    	$("#formContainer").css("display", "none");
+    	var gameDifficulty = $("#difficulty option:selected").val();
+    	var gameRounds = $("#rounds option:selected").val();
+    	settings.difficulty = gameDifficulty;
+    	settings.rounds = gameRounds;
+     	console.log(settings);
+     	getWords(settings);
 	});
 });
 
-var difficulty = {
-	difficulty: 1,
-	count: 1
-};
+
 var numGames;
 var words = [];
 
-
-function fetchWords() {
-	$.ajax({
-  	url: 'http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words',
-  	method: 'GET',
-  	data: JSON.stringify({difficulty: 1})
-	})
-		.done(function(res) {
-  		words.push(res);
-  		console.log(words);
-	})
-
-}
-
-function getWords() {
+function getWords(settings) {
 	$.ajax({
   	url: 'http://localhost:8080/words',
   	method: 'post',
-  	data: difficulty
+  	data: settings
 	})
 		.done(function(res) {
   		console.log(res);
